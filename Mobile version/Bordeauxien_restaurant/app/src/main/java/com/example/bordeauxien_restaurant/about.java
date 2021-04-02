dfffac7e3ff669844d82304579a8a651ea1ac53d
Sunwoo Chung <schung18@student.gsu.edu>
2021-04-02 00:29:11 -0400

    Sunwoo(contact/about page)

--- Mobile version/Bordeauxien_restaurant/app/src/main/java/com/example/bordeauxien_restaurant/about.java
+++ Mobile version/Bordeauxien_restaurant/app/src/main/java/com/example/bordeauxien_restaurant/about.java
@@ -2,12 +2,30 @@ package com.example.bordeauxien_restaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import static android.content.Intent.ACTION_DIAL;
 
 public class about extends AppCompatActivity {
+
+    ImageButton callButtonB, callButtonS, callButtonP, callButtonM;
+    ImageButton websB, websS, websP, websM;
+    TextView textview, textB, textS, textP, textM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,121 @@ public class about extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Textfields to edit.
+        textview = (TextView) findViewById(R.id.aboutText);
+        //type text in between ""
+        String para = "Since planting our first vines in 1962, the Chau family name has become synonymous with the highest quality French varietal wines from the Bordeaux region. For the team at Chau Family Winery, every harvest is the realization of the French dream\n"
+                +  "of Vietnamese immigrants who came to this country at the turn of the 19th century to build a family and a future.\n"
+                +  "\nToday, we believe in the importance of family more than ever – the one you’re born into and the one you choose. There is always a seat at our table and all are welcome to share in our daily celebration of family, Chau, and the bounty\n"
+                +  "of our home in Bordeaux.";
+        textview.setText(para);
+        textview.setMovementMethod(new ScrollingMovementMethod());
+
+        textB = (TextView) findViewById(R.id.textBryan);
+        //type text in between ""
+        String paraB = "about Bryan";
+        textB.setText(paraB);
+        textB.setMovementMethod(new ScrollingMovementMethod());
+
+        textS = (TextView) findViewById(R.id.textSunwoo);
+        //type text in between ""
+        String paraS = "about Sunwoo";
+        textS.setText(paraS);
+        textS.setMovementMethod(new ScrollingMovementMethod());
+
+        textP = (TextView) findViewById(R.id.textPriya);
+        //type text in between ""
+        String paraP = "about Priya";
+        textP.setText(paraP);
+        textP.setMovementMethod(new ScrollingMovementMethod());
+
+        textM = (TextView) findViewById(R.id.textMaanasa);
+        //type text in between ""
+        String paraM = "about Maanasa";
+        textM.setText(paraM);
+        textM.setMovementMethod(new ScrollingMovementMethod());
+
+        callButtonB = findViewById(R.id.phoneB);
+        callButtonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1234567890"));
+                startActivity(intent);
+            }
+        });
+
+        websB = findViewById(R.id.webB);
+        websB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/bryanchau11"));
+                startActivity(intent);
+            }
+        });
+
+        callButtonS = findViewById(R.id.phoneS);
+        callButtonS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0987654321"));
+                startActivity(intent);
+            }
+        });
+
+        websS = findViewById(R.id.webS);
+        websS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/sunwoo14"));
+                startActivity(intent);
+            }
+        });
+
+        callButtonP = findViewById(R.id.phoneP);
+        callButtonP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:6789012345"));
+                startActivity(intent);
+            }
+        });
+
+        websP = findViewById(R.id.webP);
+        websP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/hvuppu"));
+                startActivity(intent);
+            }
+        });
+
+        callButtonM = findViewById(R.id.phoneM);
+        callButtonM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1234509876"));
+                startActivity(intent);
+            }
+        });
+
+        websM = findViewById(R.id.webM);
+        websM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/lmaanasa16"));
+                startActivity(intent);
+            }
+        });
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Mobile version/Bordeauxien_restaurant/app/src/main/java/com/example/bordeauxien_restaurant/contact.java
+++ Mobile version/Bordeauxien_restaurant/app/src/main/java/com/example/bordeauxien_restaurant/contact.java
@@ -3,11 +3,23 @@ package com.example.bordeauxien_restaurant;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class contact extends AppCompatActivity {
+
+    ImageButton callButton;
+    ImageButton webSite;
+    ImageButton sendEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +29,34 @@ public class contact extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        callButton = findViewById(R.id.call);
+        callButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:6781234567"));
+                startActivity(intent);
+                return true;
+            }
+        });
+
+
+        webSite = findViewById(R.id.web);
+        webSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.bordeaux.com"));
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
